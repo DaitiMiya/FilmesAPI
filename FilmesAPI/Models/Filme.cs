82d4724d6cf5c3342807ea0311ee203ae1355674
Daiti Miyahara <daiti.miyahara@hotmail.com>
2022-11-28 00:56:12 -0300

    Add project files.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo Titulo é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        [StringLength(50, ErrorMessage = "O tamanho deste campo deve ter no máximo 50 caracteres")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1,600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+       
+    }
+}
